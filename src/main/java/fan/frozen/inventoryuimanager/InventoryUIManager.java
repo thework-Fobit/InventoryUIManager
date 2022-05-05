e2bc38d97892e5204bfa3fcdedc600fdc7379cbd
thework-fobit <734121061@qq.com>
2022-05-05 11:08:28 +0800

    update

--- src/main/java/fan/frozen/inventoryuimanager/InventoryUIManager.java
+++ src/main/java/fan/frozen/inventoryuimanager/InventoryUIManager.java
@@ -1,8 +1,6 @@
 package fan.frozen.inventoryuimanager;
 
 import fan.frozen.inventoryuimanager.commands.UIManagementCommand;
-import fan.frozen.inventoryuimanager.inventory.constructor.SinglePageInventory;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -16,9 +14,6 @@ public final class InventoryUIManager extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        for (int i = 0; i < 50; i++) {
-            new SinglePageInventory(Bukkit.createInventory(null,54),"test"+i,this);
-        }
         Objects.requireNonNull(getCommand("uimanager")).setExecutor(new UIManagementCommand());
     }
 
